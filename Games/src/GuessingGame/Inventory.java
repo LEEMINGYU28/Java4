fb9042d360d47e14da5d00d5a69f144a6179839d
dlals <dlals@DESKTOP-9BVFTED>
2023-08-28 03:57:57 +0900

    추리게임4 커밋

--- Games/src/GuessingGame/Inventory.java
+++ Games/src/GuessingGame/Inventory.java
@@ -1,7 +1,29 @@
 package GuessingGame;
 
-public class Inventory {
+import java.util.Scanner;
 
+public class Inventory 
+{
+	Scenario scenario = new Scenario();
+	Scanner scanner = new Scanner(System.in);
 	protected String[] evidences = new String[5];			
 	
-}
+	protected int inven(String player) 
+	{
+		player=scanner.nextLine();
+		if(player.equals("v")) 
+		{	
+			for(int i=0; i <evidences.length; i++) 
+			{
+				
+				if(evidences[i]!=null)
+					{
+					System.out.println(evidences[i]);
+					};
+			}
+		};
+		return 0;
+	}
+
+	
+ }
--- Games/src/GuessingGame/Main.java
+++ Games/src/GuessingGame/Main.java
@@ -2,6 +2,7 @@ package GuessingGame;
 
 import java.util.Scanner;
 
+
 public class Main {
 	
 	public static void main(String[]args) {
@@ -10,12 +11,10 @@ public class Main {
 	Suspect suspect =new Suspect();
 	Inventory inventory=new Inventory();
 	Room room =new Room();
-	Room1 room1 =new Room1();
+	Games game = new Games();
 	Scanner scanner = new Scanner(System.in);
 	String player;
-	
-	
-	
+		
 	
 	System.out.println("게임을 시작하시겠습니까? yes/no 입력");
 	player = scanner.nextLine();
@@ -37,48 +36,49 @@ public class Main {
 		if(player.equals(">>"))
 		{
 			System.out.println(room.rooms()+1+suspect.information[0]+scenario.question[0]);					
-			player=scanner.nextLine();
-			switch (player) 
-			{
-			case "침대": System.out.println(scenario.search[0]);	
-						player=scanner.nextLine();
 
-			case "책상": System.out.println(scenario.search[0]);
-						player=scanner.nextLine();
-						
-			case "액자": System.out.println(scenario.find[1]);
-						System.out.println(evidence.name[0]+evidence.description[0]
-											+scenario.question[1]);
-						player=scanner.nextLine();
-						System.out.println("저장완료/인벤토리 확인=v입력");
-						inventory.evidences[0]=evidence.name[0];
-						break;	
-			}		
-			player=scanner.nextLine();
-			if(player.equals("v")) 
-			{	
-				for(int i=0; i <inventory.evidences.length; i++) 
-				{
-					
-					if(inventory.evidences[i]!=null)
-						{
-						System.out.println(inventory.evidences[i]);						};
-				}
-			}
+			room.room1(player);
+
+
 		System.out.println(scenario.question[2]);
-		player=scanner.nextLine();
-		if(player.equals("y")) 
-		{
-		System.out.println(scenario.story[2]);
-		}
-		player=scanner.nextLine();
-		switch (player) 
-		{
-		case "1":System.out.println("투숙객A:"+suspect.reiationship[0]);
-				
-		}
 		
-	}
+		suspect.suspectA(player);
+
+		System.out.println(scenario.question[5]);
+		
+		game.progress();
+//		while(true) 
+//		{
+//			player=scanner.nextLine();
+//			switch(player) 
+//			{
+//			case "2":System.out.println(room.rooms()+2+suspect.information[1]+scenario.question[0]);
+//						room.room2(player);
+//						System.out.println(scenario.question[2]);
+//						suspect.suspectB(player);
+//						
+//			continue;
+//			case "3":System.out.println(room.rooms()+3+suspect.information[2]+scenario.question[0]);
+//						room.room3(player);
+//						System.out.println(scenario.question[2]);
+//						suspect.suspectC(player);
+//						System.out.println(scenario.question[5]);
+//			continue;
+//			case "4":System.out.println(room.rooms()+4+suspect.information[3]+scenario.question[0]);
+//						room.room4(player);
+//						System.out.println(scenario.question[2]);
+//						suspect.suspectD(player);
+//						System.out.println(scenario.question[5]);
+//			continue;
+//			case "5":System.out.println(5+suspect.information[4]+"\n열쇠사용하기 > 사용입력");}						
+//			         	player=scanner.nextLine();
+//						System.out.println(room.rooms()+5+"번방 피해자의 방이다."+scenario.question[0]);
+//						room.room5(player);
+//						System.out.println(scenario.question[6]);
+//						suspect.victim(player);
+//						System.out.println(scenario.question[5]);
+//		}
+	}	
 }	
 	else
 	{
