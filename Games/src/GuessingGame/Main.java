7bd27add2feccf9051436218f7cc23c1f3c64a5f
dlals <dlals@DESKTOP-9BVFTED>
2023-09-02 20:31:46 +0900

    추리게임 7커밋

--- Games/src/GuessingGame/Main.java
+++ Games/src/GuessingGame/Main.java
@@ -21,29 +21,7 @@ public class Main {
 	player = scanner.nextLine();
 	if(player.equals("yes")) 
 	{
-//		System.out.println("\n스토리\n"+scenario.story[0]+"\n 호텔 입장 >입력");
-//		
-//		player = scanner.nextLine();
-//		while(!player.equals(">")) 
-//		{
-//			System.out.println("> 입력");
-//			player = scanner.nextLine();
-//		}
-//		if(player.equals(">"))
-//		{
-//			System.out.println(scenario.story[1]);
-//			player = scanner.nextLine();
-//		}
-//		while(!player.equals(">>")) 
-//		{
-//			System.out.println(">> 입력");
-//			player = scanner.nextLine();
-//		}
-//		if(player.equals(">>"))
-//		{
-//			System.out.println(room.rooms()+1+suspect.information[0]+scenario.question[0]);					
-//		}	else {
-//			}
+
 			start.start();
 			
 			room.room1(player);		
