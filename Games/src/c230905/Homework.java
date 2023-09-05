c68fcf5b929ef5385b55c38e8a8ca28ffa37998e
dlals <dlals@DESKTOP-9BVFTED>
2023-09-05 22:02:29 +0900

    0905 2커밋

--- Games/src/c230905/Homework.java
+++ Games/src/c230905/Homework.java
@@ -27,9 +27,15 @@ class Stack<T>{
 	public int size() {
 		return stackList.size();
 	}
-
-
+	public T check() {
+		if (isEmpty()) {
+			return null;
+		}
+		return stackList.get(stackList.size() - 1);
+	}
 }
+
+
 
 
 public class Homework {
@@ -44,7 +50,10 @@ public class Homework {
 		stack.pop();
 		System.out.println("스택길이:"+stack.size());
 		System.out.println("스택의 맨위:"+stack.pop());
-					
+		
+		int index=4;
+		String element = stack.check();
+		System.out.println("인덱스:"+index+"요소 :"+element);
 		System.out.println("스택이 비어있는가?"+stack.isEmpty());
 	}
 }
