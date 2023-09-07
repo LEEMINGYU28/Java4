9ec1ebcad3b409104b958a1d09e60cfdbabd85d1
dlals <dlals@DESKTOP-9BVFTED>
2023-09-07 22:09:39 +0900

    0907Homework 커밋

--- Games/src/c230907/Homework.java
+++ Games/src/c230907/Homework.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.NavigableSet;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 
@@ -11,7 +12,7 @@ class Task{
 	private String todo;
 	private int priority;
 
-public Task(String todo,int priorty) {	
+public Task(String todo,int priority) {	
 	this.todo=todo;
 	this.priority=priority;
 	}	
@@ -29,33 +30,44 @@ class TodoListPriority{
 public TodoListPriority() {
 		tasks= new ArrayList<>();	
 	}
-public void addTask(String todo,int priority) {
+public void addTask(String todo,int priority) {	
+	for (Task task : tasks) {
+        if (task.getTodo().equals(todo)) {
+            System.out.println("이미 있는 할일입니다.");
+            return;
+        }
+	}
 	Task task=new Task(todo,priority);
 	tasks.add(task);
+	
+}
+private boolean isNumeric(String valueOf) {
+	// TODO Auto-generated method stub
+	return false;
 }
 public void lastTask() {
 	if(tasks.isEmpty())	{
 		System.out.println("할일이 없습니다.");
 	}else {
+		  PriorityQueue<Task> priorityQueue = new PriorityQueue<>((t1, t2) -> Integer.compare(t1.getPriority(), t2.getPriority()));
+          priorityQueue.addAll(tasks);
 		System.out.println("우선순위 순");
-		for (Task task : tasks) {
-			System.out.println("우선순위"+task.getPriority()+":"+task.getTodo());
-		}
+
+		 while (!priorityQueue.isEmpty()) {
+             Task task = priorityQueue.poll();
+             System.out.println("우선순위 " + task.getPriority() + ": " + task.getTodo());
+         }
 	}
 	
 }
 
 }
 
-	
-
-
 public class Homework {
 	
 	public static void main(String[]args) {
 		TodoListPriority todoList = new TodoListPriority();
 		Scanner scanner = new Scanner(System.in);
-		
 
 		while(true) {
 			System.out.println("할일 입력하기 1 리스트보기 2");
@@ -66,10 +78,17 @@ public class Homework {
 			String todos = scanner.nextLine();
 			System.out.println("우선순위 입력");
 			int prioritys = scanner.nextInt();
+			scanner.nextLine();
 			todoList.addTask(todos, prioritys);
 			System.out.println("할일 추가 완료");
+			break;
 			case "2" :
+			System.out.println("★오늘의 할일★");
 			todoList.lastTask();
+			break;
+			default:
+			System.out.println("올바른 명령을 입력하세요.");
+            break;
 			}
 		}
 	
