b246059e10ca4688580725753870e11b82173b57
LEEMINGYU28 <stargene250@gmail.com>
2023-11-27 18:00:51 +0900

    comment add

--- board/src/main/java/com/java4/comment/controller/CommentController.java
+++ board/src/main/java/com/java4/comment/controller/CommentController.java
@@ -1,5 +1,40 @@
 package com.java4.comment.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.java4.comment.domain.Comment;
+import com.java4.comment.service.CommentService;
+
+@Controller
+@RequestMapping("/comments")
 public class CommentController {
 
+	@Autowired
+	CommentService commentService;
+	
+	@PostMapping("add")
+	public String add(@RequestParam Map<String,String>map) {
+		Comment comment = new Comment(
+				map.get("content"),
+				Integer.parseInt(map.get("user_id")),
+				Integer.parseInt(map.get("board_id")));
+				commentService.add(comment);
+				return "redirect:/board/" + map.get("board_id");
+	};
+	
+	@GetMapping
+	@ResponseBody
+	public List<Comment> getComments(@RequestParam Map<String,String>map){
+		return commentService.getComments(Integer.parseInt(map.get("boardId")),0);
+		
+	}
 }
--- board/src/main/java/com/java4/comment/service/CommentService.java
+++ board/src/main/java/com/java4/comment/service/CommentService.java
@@ -1,5 +1,41 @@
 package com.java4.comment.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.java4.comment.dao.CommentDAO;
+import com.java4.comment.domain.Comment;
+
+@Service
 public class CommentService {
 
+	@Autowired
+	CommentDAO commentDao;
+	
+	public void add(Comment comment) {
+		commentDao.add(comment);
+	}
+	public List<Comment> getComments(int boardId,int start){
+		List<Comment> list = commentDao.getParent(boardId, start);
+//		for(int i = 0; i<list.size(); ++i) {
+//			list.get(i).getId();
+//			list.get(i).setChildren(
+//					commentDao.getChildren(
+//							boardId, list.get(i).getId()));
+//		}
+		list.forEach((item)->{
+			item.setChildren(getChildren(boardId,item));
+		});
+		return list;
+	}
+	
+	private List<Comment> getChildren(int boardId,Comment comment){
+		List<Comment> list = commentDao.getChildren(boardId,comment.getId());
+		list.forEach((item)->{
+			item.setChildren(getChildren(boardId,item));
+		});
+		return list;
+	}
 }
